5e58188f447bc6273a2ae0a53181cbc90d8f68d6
Malkon F <sudo777@protonmail.com>
2019-04-02 14:11:33 -0300

    Criando operações de instanciação

--- src/main/java/me/malkon/cursomc/CursomcApplication.java
+++ src/main/java/me/malkon/cursomc/CursomcApplication.java
@@ -1,13 +1,27 @@
 package me.malkon.cursomc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import me.malkon.cursomc.domain.Categoria;
+import me.malkon.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+	
+	@Override
+	public void run(String ... args) throws Exception{
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
 	}
 
 }
