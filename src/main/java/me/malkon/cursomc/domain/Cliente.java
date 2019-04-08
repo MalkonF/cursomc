97ecc713fcbaf65b6561fc462cf7e09bb336776b
Malkon F <sudo777@protonmail.com>
2019-04-08 18:13:36 -0300

    Criação do EndPoint clientes

--- src/main/java/me/malkon/cursomc/domain/Cliente.java
+++ src/main/java/me/malkon/cursomc/domain/Cliente.java
@@ -14,6 +14,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import me.malkon.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -29,6 +31,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 
+	@JsonManagedReference //cli serializa vários endereços, mas end n podem serializar mais de 1 cli
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
---
+++ src/main/java/me/malkon/cursomc/services/ClienteServices.java
@@ -0,0 +1,25 @@
+package me.malkon.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import me.malkon.cursomc.domain.Cliente;
+import me.malkon.cursomc.repositories.ClienteRepository;
+import me.malkon.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteServices {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+}
