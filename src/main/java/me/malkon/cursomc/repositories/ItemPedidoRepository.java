7bf47ccc5dc78342418cda0469131a209cf79a3c
Malkon F <sudo777@protonmail.com>
2019-04-09 14:13:24 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/me/malkon/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package me.malkon.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.malkon.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
