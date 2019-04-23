b483119c8f36a8b178d18ed4ddd1adba9eedabb8
Malkon F <sudo777@protonmail.com>
2019-04-23 16:36:32 -0300

    Inserindo nova categoria com POST

--- src/main/java/me/malkon/cursomc/resources/CategoriaResource.java
+++ src/main/java/me/malkon/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package me.malkon.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import me.malkon.cursomc.domain.Categoria;
 import me.malkon.cursomc.services.CategoriaServices;
@@ -24,6 +28,16 @@ public class CategoriaResource {
 
 		return ResponseEntity.ok().body(obj);
 
+	}
+
+	// ResponseEntity recebe um retorno de resposta http e void diz que o corpo da
+	// resposta é vazio
+	@RequestMapping(method = RequestMethod.POST) // método vai ser mapeado no endpoint categorias tipo POST
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {// obj vai ser construido a partir dos dados JSON.
+																	// JSON vai ser convertido p obj java
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build(); // retorna http status created
 	}
 
 }
--- src/main/java/me/malkon/cursomc/services/CategoriaServices.java
+++ src/main/java/me/malkon/cursomc/services/CategoriaServices.java
@@ -11,17 +11,21 @@ import me.malkon.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaServices {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
-		
+
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		
-	
-		
-		}
+
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);// se for novo obj tem que ter o id nulo, se já tiver id, vai ser considerado
+						// como atualização
+		return repo.save(obj);
+	}
 }
