0b1637ccf0f0ba71120afb0b424170106c71c610
Malkon F <sudo777@protonmail.com>
2019-04-29 10:43:40 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/me/malkon/cursomc/resources/CategoriaResource.java
+++ src/main/java/me/malkon/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -64,6 +66,22 @@ public class CategoriaResource {
 		// converte lista para listaDto
 		return ResponseEntity.ok().body(listDto);
 
+	}
+
+	// faz o endpoint p chamar o método p listar as categorias paginadas.
+	@RequestMapping(value = "/page", method = RequestMethod.GET) // concatena /pages com /categorias
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, // parametros opcionais,
+																								// nome parametro
+																								// linesPerPage, valor
+																								// default 24. Se n
+																								// colocar nada esse
+																								// valor será usado
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/me/malkon/cursomc/services/CategoriaServices.java
+++ src/main/java/me/malkon/cursomc/services/CategoriaServices.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import me.malkon.cursomc.domain.Categoria;
@@ -48,5 +51,11 @@ public class CategoriaServices {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	// recurso de paginação. direction tem que ser convertido de string p Direction
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);// retorna a página
 	}
 }
