7457c23ccae5f077f336a25c54f3a858fede8337
sudo777 <sudo777@protonmail.com>
2019-06-26 21:58:52 +0000

    Validação customizada: email não repetido na atualização do cliente

--- src/main/java/me/malkon/cursomc/dto/ClienteDTO.java
+++ src/main/java/me/malkon/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import me.malkon.cursomc.domain.Cliente;
+import me.malkon.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
---
+++ src/main/java/me/malkon/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,68 @@
+package me.malkon.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import me.malkon.cursomc.domain.Cliente;
+import me.malkon.cursomc.dto.ClienteDTO;
+import me.malkon.cursomc.repositories.ClienteRepository;
+import me.malkon.cursomc.resources.exception.FieldMessage;
+
+/*É usado o ClienteDTO ao invés do ClienteNewDTO pq este dto que é utilizado p atualizar*/
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	/*
+	 * Qnd se faz uma atualização n é passado o id no corpo do dto e sim na URI.
+	 * Como vou fazer pra pegar o id da uri dentro da classe validator? Vou inetar
+	 * no validator um objeto httprequest , esse objeto tem um metodo q permite
+	 * obter o parametro da uri.
+	 **/
+	@Autowired
+	private HttpServletRequest request;
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	/*
+	 * Como é uma atualização, o e-mail tem que ser o mesmo email que já está
+	 * cadastrado, se for outro email ele não pode estar cadastrado no banco. Para
+	 * isso vai usar o id
+	 */
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		/*
+		 * Qd se faz a requisição ela tem vários atributos e esses atributos são
+		 * armazenados dentro de um Map get pega o id que veio nos atributos que estão
+		 * no map
+		 */
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request
+				.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+
+		List<FieldMessage> list = new ArrayList<>();
+		/* Se existir um email e a id desse email n for a minha, apresentar erro */
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId))
+			list.add(new FieldMessage("email", "Email já existente"));
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
