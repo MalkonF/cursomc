8cfb7af12a7504ecd22ae7226ad63618a482aa19
sudo777 <sudo777@protonmail.com>
2019-07-01 15:42:46 -0300

    Busca produtos por nomes/categorias

--- src/main/java/me/malkon/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/me/malkon/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,30 @@
 package me.malkon.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import me.malkon.cursomc.domain.Categoria;
 import me.malkon.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	// não é necessário fazer uma transação, somente consulta
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome,
+			@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 
 }
+/*
+ * @Param - vai pegar a variável e jogar na query da consulta @Query
+ * 
+ * Se vc retirar a anotação @Query o spring vai continuar buscando no banco de
+ * dados através do nome que colocamos no método.
+ */
