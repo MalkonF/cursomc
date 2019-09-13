c03595bebd589d178009e838e35bfbe49573f10a
anonymous <sudo777@protonmail.com>
2019-09-13 02:24:42 -0300

    Restricao de conteudo - Cliente so recupera seus pedidos

--- src/main/java/me/malkon/cursomc/repositories/PedidoRepository.java
+++ src/main/java/me/malkon/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package me.malkon.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import me.malkon.cursomc.domain.Cliente;
 import me.malkon.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+	@Transactional(readOnly = true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/me/malkon/cursomc/services/PedidoServices.java
+++ src/main/java/me/malkon/cursomc/services/PedidoServices.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import me.malkon.cursomc.domain.Cliente;
 import me.malkon.cursomc.domain.ItemPedido;
 import me.malkon.cursomc.domain.PagamentoComBoleto;
 import me.malkon.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import me.malkon.cursomc.domain.enums.EstadoPagamento;
 import me.malkon.cursomc.repositories.ItemPedidoRepository;
 import me.malkon.cursomc.repositories.PagamentoRepository;
 import me.malkon.cursomc.repositories.PedidoRepository;
+import me.malkon.cursomc.resources.exception.AuthorizationException;
+import me.malkon.cursomc.security.UserSS;
 import me.malkon.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,5 +74,16 @@ public class PedidoServices {
 		itemPedidoRepository.saveAll(obj.getItens());// repository é capaz de salvar listas
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+	// Verifica quem é o user logado e busca os pedidos somente dele
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) { // n está autenticado?
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
